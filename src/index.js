dbf7b600f54048cf9b66fe9bcd0556e5f3391333
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-11-03 17:29:46 -0300

    agregue modificaciones en archivo de vercel

--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
 import express from "express";
 import morgan from "morgan";
-import cors from "cors"
+import cors from "cors";
 import path from "path";
-import "./database"
+import "./database";
 import productosRouter from "./routes/productos.routes";
 
 // crear uns instacia e express
 const app = express();
 
-
-
-
-
-
 // creamos una variable
-app.set("port",process.env.PORT|| 4000);
+app.set("port", process.env.PORT || 4000);
 // usar el puerto
-app.listen(app.get("port"),()=>{
-    console.log("Estoy en el puerto "+app.get("port"));
-})
+app.listen(app.get("port"), () => {
+  console.log("Estoy en el puerto " + app.get("port"));
+});
 
 // middlewares : funciones que se ejecutan antes de las rutas
 // da informacion extra en la terminal
@@ -27,23 +22,21 @@ app.use(morgan("dev"));
 app.use(cors());
 // middlewares para interprestar los objetos json
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 // cargar un archivo estatico
-app.use(express.static(path.join(__dirname,"../public")))
+app.use(express.static(path.join(__dirname, "../public")));
 // console.log(path.join(__dirname,"../public"))
-
-
 
 // rutas: nombre de dominio + -----
 //  http://localhost:4000/
-app.get("/productos",(req , res)=>{
-res.send("aqui tengo que retornar un arreglo e productos")
-})
-app.post("/prueba",(req , res)=>{
-res.send("Esto es una prueba de la peticion GET")
-})
-app.get("/productos2",(req , res)=>{
-res.send("aqui devolvemos un producto")
-})
+app.get("/productos", (req, res) => {
+  res.send("aqui tengo que retornar un arreglo e productos");
+});
+app.post("/prueba", (req, res) => {
+  res.send("Esto es una prueba de la peticion GET");
+});
+app.get("/productos2", (req, res) => {
+  res.send("aqui devolvemos un producto");
+});
 
-app.use("/apicafe",productosRouter)
+app.use("/apicafe", productosRouter);
